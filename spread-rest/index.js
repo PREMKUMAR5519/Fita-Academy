02bdfa275c87dc6fc0b579cedb3973dfc24fc868
PREMKUMAR5519 <premkumarananthan1@gmail.com>
2025-06-19 19:51:20 +0530

    operators

---
+++ spread-rest/index.js
@@ -0,0 +1,151 @@
+//spread
+//  let redBasket = ["apple","bannana","mango","orange","grapes"]
+// let greenBasket = ["blueberry","grapes","stawberry"]
+// let whiteBasket =[...redBasket,...greenBasket]
+// console.log(whiteBasket)
+
+
+
+////rest
+
+// function tweet(...values){
+
+//     console.log(a)
+//     console.log(b)
+//     console.log(values[0])
+//     console.log(values[1])
+//     console.log(values[2])
+// }
+
+// tweet(5,4,5,7,8)
+
+
+
+//assignment operators
+
+// let a = 7
+//  a-=3  //-----> a = a + 3
+//  console.log(a)
+
+
+//comparision opeerators 
+
+
+// let a = 5
+// let b = "5"
+// console.log(a===b)
+
+// let a = 5
+// let b = "5"
+// console.log(a!==b)
+
+
+// let a = 5
+// let b = 6
+// let c =  a==b && a==b
+// console.log(c)
+
+
+// let a = 10
+// let b  = 55;
+// let c = true || true
+
+// console.log(c)
+
+
+// let a = false
+
+// let b = !a
+// console.log(b)
+
+
+
+// let a = "hello"
+// let b = "Welcome"
+
+// console.log(a+b)
+
+
+// let a = 5
+
+// let b = 2
+
+// let c = a < b ? "brigger":"smaller"  //----------> condition ? truevalue : falsevalue
+// console.log(c)
+
+
+
+//conditional statment
+
+// if statement
+
+
+// if(condition){
+//     //block of codes
+// }
+
+// let a = 5;
+// let b = 6;
+// if(b<a){
+//     console.log(b+'is greater than'+a)
+// }
+
+
+
+// if else staement
+
+// let a = 7;
+// let b = 9;
+
+// if(a>b){
+//   console.log(a+'is greater than'+b)
+// }else{
+//   console.log(a+'is smaller than'+b)
+
+// }
+
+
+// if else if else 
+
+
+// let a = 5;
+// let b = 7;
+
+// if(a < b){
+//   console.log(a+'is lesser  than'+b)
+// }
+// else if(a<=b){
+//     console.log(" a is equa or lesser than to b")
+// }
+// else{
+//     console.log("a is smaller than b")
+// }
+
+
+
+/// switch case
+
+// let a = "may";
+
+// switch(a){
+//     case "june":
+//         console.log("school is openning....")
+//         break;
+//     case "may":
+//         console.log("school is holiday")
+//         break;
+//     default:
+//         console.log("this is month of"+ a)
+// }
+
+//tmpelate literals
+
+
+let a = 6;
+let b = 7;
+
+// console.log(`${a} is lesser than ${b}`)
+
+
+
+console.log(`${a} is lesser than ${b}`)
