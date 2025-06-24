5ddbecd243fc7bad06146adaff8000843e4e2362
PREMKUMAR5519 <premkumarananthan1@gmail.com>
2025-06-24 19:33:55 +0530

    synchronous

---
+++ advancedjs/index.js
@@ -0,0 +1,12 @@
+// console.log("Hot water")
+
+setTimeout(() => {
+    console.log("hot water")
+}, 100);
+
+
+setTimeout(() => {
+console.log("Shirt ironing")
+}, 2000);
+
+console.log("Have to cook")
