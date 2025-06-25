f8fd896ed949151707e7a12260d1118460e856a7
PREMKUMAR5519 <premkumarananthan1@gmail.com>
2025-06-25 19:27:40 +0530

    promise,async,await

--- advancedjs/index.js
+++ advancedjs/index.js
@@ -1,12 +1,49 @@
 // console.log("Hot water")
 
-setTimeout(() => {
-    console.log("hot water")
-}, 100);
+// setTimeout(() => {
+//     console.log("hot water")
+// }, 100);
 
 
-setTimeout(() => {
-console.log("Shirt ironing")
-}, 2000);
+// setTimeout(() => {
+// console.log("Shirt ironing")
+// }, 2000);
 
-console.log("Have to cook")
+// console.log("Have to cook")
+
+
+//promise
+
+
+const value = false;
+function check() {
+
+    return new Promise((resolve, reject) => {
+        if (value) {
+            resolve("value is true")
+        } else {
+            reject("value is false")
+        }
+    })
+}
+
+
+// console.log(check())
+
+
+// check().then((message)=> console.log(message)).catch((error)=> console.log(error))
+
+
+async function getResults() {
+    try {
+        let result = await check()
+        console.log(result)
+    } catch (error) {
+        console.log(error)
+        
+    }finally{
+        console.log("function is finished")
+    }
+}
+
+getResults()
