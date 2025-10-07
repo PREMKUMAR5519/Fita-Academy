d01a0a2a322bb5c8bcb1dc824553d292be6a7b05
PREMKUMAR5519 <premkumarananthan1@gmail.com>
2025-10-07 19:04:11 +0530

    changes

---
+++ arunprasad/bootstrap/bootstrap/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='container'>
+      <h2 className='bg-danger'>hello</h2>
+
+      <div className='row'>
+        <div className='col-4 bg-primary'>
+          grid 1
+        </div>
+        <div className='col-4 bg-warning'>
+          grid 1
+        </div>
+        <div className='col-4 bg-danger'>
+          grid 1
+        </div>
+
+      </div>
+    </div>
+
+  )
+}
+
+export default App
