c488900baab02bd8c16453c82f003b8b1f7b110c
PREMKUMAR5519 <premkumarananthan1@gmail.com>
2025-10-07 19:06:12 +0530

    changes

--- arunprasad/bootstrap/bootstrap/src/App.jsx
+++ arunprasad/bootstrap/bootstrap/src/App.jsx
@@ -15,8 +15,16 @@ function App() {
         <div className='col-4 bg-danger'>
           grid 1
         </div>
-
+<div class="card p-2" style={{width: "18rem"}}>
+  <img src="..." class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+    <a href="#" class="btn btn-warning">Go somewhere</a>
+  </div>
+</div>
       </div>
+      
     </div>
 
   )
